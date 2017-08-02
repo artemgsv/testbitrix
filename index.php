04cb2a508fcb9dc8f79412d724ad39169ea8223e
Artem Gusev <ar.gusev@yandex.ru>
2017-08-02 19:06:44 +0400

    Добавление инфоблока

--- index.php
+++ index.php
@@ -3,4 +3,6 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle(" ");
 ?>
     <p>Добро пожаловать!</p>
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+<?
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
--- urlrewrite.php
+++ urlrewrite.php
@@ -1,17 +1,9 @@
 <?
 $arUrlRewrite = array(
-	array(
-		"CONDITION" => "#^/photo/#",
-		"RULE" => "",
-		"ID" => "bitrix:photogallery",
-		"PATH" => "/photo.php",
-	),
-	array(
-		"CONDITION" => "#^/#",
-		"RULE" => "",
-		"ID" => "bitrix:blog",
-		"PATH" => "/index.php",
-	),
+        array(
+            "CONDITION" => "#^/vacancy/#",
+            "RULE" => "",
+            "PATH" => "/vacancies.php",
+            "ID" => "my:vacancy",
+        ),
 );
-
-?>
